73e4106d952448cc01cc138c5c43278c913bb678
zoozq <romahome2015@mail.ru>
2022-01-26 00:02:11 +0300

    fix lw4

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
-    case '/':
+    case '/' :
         require __DIR__ . '/public/index.php';
         break;
-    case '':
+    case '' :
         require __DIR__ . '/public/index.php';
         break;
+    case '/authentication' :
+        require __DIR__ . '/public/authentication.php';
+        break;
+    case '/requests' :
+        require __DIR__ . '/public/requests.php';
+        break;
+        
     default:
         http_response_code(404);
         require __DIR__ . '/public/404.php';
